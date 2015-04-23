e338a30f568b354f2aea8282f78b2b08bccc95b3
Evan Butler & Kelly de Vries <student@epicodus.com>
2015-04-23 15:55:16 -0700

    add time elapsed ranking system.

--- services/ThreadsFactory.js
+++ services/ThreadsFactory.js
@@ -2,9 +2,16 @@ hackerNews.factory('ThreadsFactory', function ThreadsFactory() {
   var factory = {};
   factory.threads = [];
   factory.addThread = function() {
-    factory.threads.push({ name: factory.threadName, id: factory.threads.length + 1, comments: [], url: factory.threadUrl, votes: 0, date: new Date() });
+    factory.threads.push({ name: factory.threadName, id: factory.threads.length + 1, comments: [], url: factory.threadUrl, votes: 0, date: new Date(), ranking: 0, days: 0 });
     factory.threadName = null;
     factory.threadUrl = null;
+
+    // var timePassed = setInterval(function() { this.days -= 10 }, 10000);
+    // var ranking = (this.factory.threads.votes) + (timePassed);
+    // if (this.factory.threads.ranking < 0) {
+    //   var index = factory.threads.indexOf(thread);
+    //   factory.threads.splice(index, 1);
+    // }
   };
 
   factory.plusVote = function(item) {
@@ -14,6 +21,13 @@ hackerNews.factory('ThreadsFactory', function ThreadsFactory() {
   factory.minusVote = function(item) {
     item.votes -= 1;
   };
+
+  factory.checkThread = function(item) {
+    var currentDate = new Date();
+    var elapsedTime = currentDate - item.date;
+    return elapsedTime < 10000;
+  };
+
 
   return factory;
 });
